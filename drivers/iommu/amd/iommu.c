b8e138a2886767bb30180470aabeb0a8cb9c7135
Jason Zeng <jason.zeng@intel.com>
2020-09-02 10:37:54 +0800

    iommu: introduce IOMMU_CAP_FGSP for fine-grained-super-page support
    Vfio driver tries to set up a two-page mapping and unmap it to see
    if the underlying IOMMU hardware supports fine-grained-super-page
    or not. However this is not viable for keepalive devices use case,
    because it may clobber existing keepalive IOMMU page table.
    
    Introduce an IOMMU capability for fine-grained-super-page so that
    vfio layer can retrieve this from underlying IOMMU driver.
    
    Signed-off-by: Jason Zeng <jason.zeng@intel.com>

--- drivers/iommu/amd/iommu.c
+++ drivers/iommu/amd/iommu.c
@@ -2566,6 +2566,8 @@ static bool amd_iommu_capable(enum iommu_cap cap)
 		return (irq_remapping_enabled == 1);
 	case IOMMU_CAP_NOEXEC:
 		return false;
+	case IOMMU_CAP_FGSP:
+		return true;
 	default:
 		break;
 	}
